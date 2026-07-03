6fc424d3e3889fa46f01fbc7245c957ac55797d5
Aksh002 <akshitgangwar02@gmail.com>
2026-07-04 02:33:11 +0530

    backend v1

--- Backend/Routes/1-main.tsx
+++ Backend/Routes/1-main.tsx
@@ -3,10 +3,19 @@ const Main = new Hono();
 
 import User from "./2-user";
 import Blog from "./3-blog";
+import Posts from "./4-posts";
+import UsersRoute from "./5-users";
+import SeriesRoute from "./6-series";
+import Platform from "./7-platform";
 
 Main.route("/user",User)
 
 Main.route('/blog',Blog)
+
+Main.route("/posts", Posts)
+Main.route("/users", UsersRoute)
+Main.route("/series", SeriesRoute)
+Main.route("/", Platform)
 
 export default Main;
 
