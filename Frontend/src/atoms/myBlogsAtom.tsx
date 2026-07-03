7d74f4aa07267c09542f3e360bba923ffa0ea0e8
Aksh002 <akshitgangwar02@gmail.com>
2026-07-04 02:38:42 +0530

    some atoms update

--- Frontend/src/atoms/myBlogsAtom.tsx
+++ Frontend/src/atoms/myBlogsAtom.tsx
@@ -1,36 +1,12 @@
-import axios from "axios";
-import { atom, atomFamily, selector, selectorFamily, useRecoilValue } from "recoil";
-import { tokenAtom } from "./tokenAt"
+import { atom } from "recoil";
+import type { Post } from "../types";
 
-export const viewMyBlogsAtom=atom({
-    key:"viewMyBlogsAtom",
-    default:false
-})
+export const viewMyBlogsAtom = atom<boolean>({
+  key: "viewMyBlogsAtom",
+  default: false,
+});
 
-
-export const myBlogsAtom=atom({
-    key:"myBlogsAtom",
-    default:selector({
-        key:"myBlogsSelector",
-        get:async({get})=>{
-            const token=get(tokenAtom)
-            if (!token){
-                return [];
-            }
-            try{
-                const response=await axios.get("https://backend.akshitgangwar02.workers.dev/api/v1/blog/myPosts",{
-                    headers:{
-                        Authorization:`Bearer ${token}`
-                    }
-                })
-                return response.data.allPost || []
-                //const allBlogs=response.data.allPost || []
-                // const blogsIds=allBlogs.map((post)=>post.id)
-                // return blogsIds
-            }catch(error){
-                console.error("Error fetching blogs:", error);
-                return [];
-            }
-        }
-    })
-})
+export const myBlogsAtom = atom<Post[]>({
+  key: "myBlogsAtom",
+  default: [],
+});
